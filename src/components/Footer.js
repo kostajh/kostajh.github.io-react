8b43ed3f85b12f2d698f616e5dff3a7246b8fc6c
Kosta Harlan <kosta@fastmail.com>
2017-05-04 22:42:25 -0400

    Rewrite footer

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,12 +1,31 @@
 import React from 'react';
 
+const FooterProperties = {
+  email: {
+    url: "mailto:kosta@fastmail.com",
+    title: "kosta@fastmail.com"
+  },
+  drupal: {
+    url: "https://drupal.org/u/kostajh",
+    title: "Drupal.org"
+  },
+  github: {
+    url: "https://github.com/kostajh",
+    title: "GitHub"
+  },
+  atletico: {
+    url: "https://www.durhamatletico",
+    title: "Durham Atlético"
+  }
+};
+
 const Footer = () => (
   <footer className="pv4 ph3 ph5-m ph6-l mid-gray">
-    <small className="f6 db tc"><a className="dib ph2 link mid-gray dim" href="mailto:kosta@fastmail.com">kosta@fastmail.com</a></small>
+    <small className="f6 db tc"><a className="dib ph2 link mid-gray dim" href={FooterProperties.email.url}>{FooterProperties.email.title}</a></small>
     <div className="tc mt3">
-      <a href="https://drupal.org/u/kostajh"    title="Drupal.org" className="f6 dib ph2 link mid-gray dim">Drupal.org</a>
-      <a href="https://github.com/kostajh"  title="GitHub" className="f6 dib ph2 link mid-gray dim">GitHub</a>
-      <a href="https://www.durhamatletico.com" title="Durham Atletico" className="f6 dib ph2 link mid-gray dim">Durham Atletico</a>
+      <a href={FooterProperties.drupal.url} title={FooterProperties.drupal.title} className="f6 dib ph2 link mid-gray dim">{FooterProperties.drupal.title}</a>
+      <a href={FooterProperties.github.url}  title={FooterProperties.github.title} className="f6 dib ph2 link mid-gray dim">{FooterProperties.github.title}</a>
+      <a href={FooterProperties.atletico.url} title={FooterProperties.atletico.title} className="f6 dib ph2 link mid-gray dim">{FooterProperties.atletico.title}</a>
     </div>
   </footer>
 
