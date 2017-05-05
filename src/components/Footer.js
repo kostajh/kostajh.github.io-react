7bb6193e6ea515e1923fef675489f9c9102609a1
Kosta Harlan <kosta@fastmail.com>
2017-05-04 21:25:03 -0400

    Add footer

---
+++ src/components/Footer.js
@@ -0,0 +1,15 @@
+import React from 'react';
+
+const Footer = () => (
+  <footer className="pv4 ph3 ph5-m ph6-l mid-gray">
+    <small className="f6 db tc">Contact at <b className="ttu">[first-name]@fastmail.com</b></small>
+    <div className="tc mt3">
+      <a href="https://drupal.org/u/kostajh"    title="Drupal.org" className="f6 dib ph2 link mid-gray dim">Drupal.org</a>
+      <a href="https://github.com/kostajh"  title="GitHub" className="f6 dib ph2 link mid-gray dim">GitHub</a>
+      <a href="https://www.durhamatletico.com" title="Durham Atletico" className="f6 dib ph2 link mid-gray dim">Durham Atletico</a>
+    </div>
+  </footer>
+
+);
+
+export default Footer;
